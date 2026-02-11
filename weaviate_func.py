2bce6c945dcf1c02f0c8242070ba94384dd9447f
era011 <erlan.chodoev@gmail.com>
2026-02-11 11:57:33 +0600

    add langgraph

--- weaviate_func.py
+++ weaviate_func.py
@@ -1,32 +1,18 @@
 import weaviate
 import os
 
-# Connect to the Weaviate instance
-# Create the client
-url1=os.getenv("WEAVIATE_URL")
-
-print(url1)
-print('ssssssssssssssssss')
-client = weaviate.Client(
-    url=url1,
-    startup_period=15
-)
-
-# Test the connection
-if client.is_ready():
-    print("Connected to Weaviate successfully!")
-else:
-    print("Failed to connect to Weaviate.")
-
-
-def search(query):
-    result = client.query.get("Document", ["content", "source"]) \
-        .with_near_text({
-            "concepts": [query]
-        }) \
-        .with_limit(3) \
-        .do()
-
-    chunks = result["data"]["Get"]["PDFChunk"]
-
-print(search('что такое Заявка?'))    
+def search(query,limit=5):
+    client= weaviate.Client(
+        url=os.getenv("WEAVIATE_URL"),
+    )
+    try:
+        if client.is_ready():
+            result = client.query.get("Document", ["content", "source",'url']) \
+                .with_near_text({
+                    "concepts": [query]
+                }) \
+                .with_limit(limit) \
+                .do()
+        return result    
+    except Exception as e:
+        print(f"Ошибка: {e}")
